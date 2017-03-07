e3a45ca5c9de200d2d0121ee65d06b57a00feb89
Yuxiao Zhang <ttcmk@yahoo.com>
2017-03-06 19:09:29 -0500

    Create binarysearch.py

---
+++ binarysearch.py
@@ -0,0 +1,18 @@
+class Binarysearch:
+    def binarysearch(self, nums, target):
+        if len(nums) == 0:
+            return -1
+        
+        start, end = 0, len(nums)-1
+        while start + 1 < end:
+            mid = (start + end) / 2
+            if nums[mid] < target:
+                start = mid
+            else:
+                end = mid
+        
+        if nums[start] == target:
+            return start
+        if nums[end] == target:
+            return end
+        return -1
