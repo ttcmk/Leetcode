7b9d0fb202fdba62a8c173315b56ff6883058eb1
Yuxiao Zhang <ttcmk@yahoo.com>
2017-03-27 14:07:12 -0400

    Update 62. Unique Paths.java

--- Dynamic Programming/62. Unique Paths.java
+++ Dynamic Programming/62. Unique Paths.java
@@ -10,23 +10,26 @@ public class Solution {
     public int uniquePaths(int m, int n) {
         int res = 0;
         int[][] path = new int[m][n];
-        
+
         if(m == 1 || n == 1){
             return 1;
         }
-        for(int i=1;i<m;i++){
-            path[i][0] = 1;
-        }
-        for(int i=1;i<n;i++){
-            path[0][i] = 1;
-        }
-        
-        for(int i=1;i<m;i++){
-            for(int j=1;j<n;j++){
-                path[i][j] = path[i][j-1] + path[i-1][j];
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i == 0){
+                    path[0][j] = 1;
+                }
+                else if(j == 0){
+                    path[i][0] = 1;
+                }
+                else{
+                    path[i][j] = path[i][j-1] + path[i-1][j];
+                }
             }
         }
-        
+
         return path[m-1][n-1];
     }
 }
+
